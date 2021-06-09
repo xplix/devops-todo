45ad3744462edf359c27cb231079b89355849eaf
xplix <70627460+xplix@users.noreply.github.com>
2021-06-09 13:34:43 +0200

    Add files via upload

---
+++ test/test.js
@@ -0,0 +1,7 @@
+const assert = require('chai').assert;
+const app = require('../app');
+describe('App', function(){
+    it('app should return hello', function(){
+        assert.equal(app(), 'hello');
+    });
+});
